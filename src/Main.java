d4f3bd1b2664b2aecc36b13971821be3618b51b2
Ethan <ethanxxjohnson@gmail.com>
2025-02-14 10:20:11 -0500

    final submission

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your birth month 1-12");
+
+        if (in.hasNextInt()){
+            int birthMonth = in.nextInt();
+            if (birthMonth >= 1 && birthMonth <= 12){
+                System.out.println("Your birth month is: " + birthMonth);
+            } else{
+                System.out.println("You entered an incorrect month value: " + birthMonth);
+            }
+        } else{
+            System.out.println("Please enter an integer value");
+        }
     }
 }
